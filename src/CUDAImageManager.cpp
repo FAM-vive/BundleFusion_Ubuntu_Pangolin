5b9b25b917857407c1da823167e220bcb096ca7b
FAM-vive <46038444+FAM-vive@users.noreply.github.com>
2022-01-06 22:51:04 +0800

    Update CUDAImageManager.cpp

--- src/CUDAImageManager.cpp
+++ src/CUDAImageManager.cpp
@@ -24,6 +24,28 @@ bool CUDAImageManager::process ( cv::Mat& rgb, cv::Mat& depth )
     {
         return false;
     }
+    std::cout<<rgb.size()<<std::endl;
+    //semantic start
+    //mask 保存在maskrcnn.results
+    maskrcnn.infer(rgb);
+    // maskrcnnMask = maskrcnn.results;
+    // slicTools.
+    setmaskrcnnMask(maskrcnn.results);
+    if(maskrcnn.getMaskNum()>0)
+    {
+			cv::Mat img1(240, 320, CV_8UC1);
+			memcpy ( img1.data,maskrcnnMask , sizeof (uint8_t ) *320*240 );
+            cv::imwrite("before.png",img1*200);
+    }
+    // slicTools.rgbdSuperPixelSeg(rgb,depth,finalSPixel);
+    // maskSuperPixelFilter_OverSeg(1200, finalSPixel, maskrcnn.getMaskNum());
+    if(maskrcnn.getMaskNum()>0)
+    {
+			cv::Mat img1(240, 320, CV_8UC1);
+			memcpy ( img1.data,maskrcnnMask , sizeof (uint8_t ) *320*240 );
+            cv::imwrite("after.png",img1*200);
+    }
+    //semantic end
 
     if ( m_currFrame + 1 > GlobalBundlingState::get().s_maxNumImages * GlobalBundlingState::get().s_submapSize )
     {
@@ -41,6 +63,10 @@ bool CUDAImageManager::process ( cv::Mat& rgb, cv::Mat& depth )
     m_data.push_back ( ManagedRGBDInputFrame() );
     ManagedRGBDInputFrame& frame = m_data.back();
     frame.alloc();
+    //添加mask
+    // frame.setmask(maskrcnn.results);
+    frame.setmasknew(maskrcnn.getMaskNum(),maskrcnnMask,scores);
+
 
     const unsigned int bufferDimColorInput = m_RGBDSensor->getColorWidth() *m_RGBDSensor->getColorHeight();
     MLIB_CUDA_SAFE_CALL ( cudaMemcpy ( d_colorInput, m_RGBDSensor->getColorRGBX(), sizeof ( uchar4 ) *bufferDimColorInput, cudaMemcpyHostToDevice ) );
@@ -149,4 +175,72 @@ bool CUDAImageManager::process ( cv::Mat& rgb, cv::Mat& depth )
 
     m_currFrame++;
     return true;
+}
+
+
+// 用rgbd超像素分割的结果去优化原始的maskrcnn的mask
+void CUDAImageManager::maskSuperPixelFilter_OverSeg(int spNum, int *finalSPixel, int resultMasks_Num)
+{
+	//Step 1 count pixel num。NumMatrix就是用来存每个mask和每个超像素重叠的像素数量，最后的spNum个元素存的是每个超像素标号的像素数
+	int* PixelValueOfEachMask = (int*)malloc(resultMasks_Num * sizeof(int)); // 用于存每个mask的标签值
+	int* NumMatrix = (int*)malloc((resultMasks_Num + 1)*spNum*sizeof(int));  // (mask的数量+1) * 超像素数量，多个1是用来存每个超像素的像素数量
+	memset(NumMatrix, 0, (resultMasks_Num + 1)*spNum*sizeof(int));
+	for (int x = 0; x<320; x++)  // 遍历超像素上的每一个像素点
+	{
+		for (int y = 0; y<240; y++)
+		{
+			int id = finalSPixel[y*320 + x];  // 像素坐标为(x,y)的超像素标号
+			if (id >= spNum || id<0) 
+                continue;
+			//last row(pointNum of each superPixel) 统计每个超像素的像素数量
+			NumMatrix[resultMasks_Num*spNum + id] ++;
+
+			for (int i = 0; i<resultMasks_Num; i++)  // 遍历masks上每一个对应位置上的像素点
+			{
+				if (maskrcnnMask[i*320*240 + y*320 + x])  // 如果mask对应像素有值
+				{
+					PixelValueOfEachMask[i] = maskrcnnMask[i*320*240 + y*320 + x];
+					//pointNum of each Instance
+					NumMatrix[i*spNum + id] ++;  // 第i个mask和第id个超像素的重叠数加1
+				}
+			}
+		}
+	}
+
+	// Step 2
+	for (int x = 0; x<320; x++)  // 遍历超像素上的每一个像素点
+	{
+		for (int y = 0; y<240; y++)
+		{
+			int id = finalSPixel[y*320 + x];
+			if (id >= spNum || id<0)  // 若像素点的超像素标号处于正常范围之外，该点的所有mask都直接置为0
+			{
+                // std::cout<<"error id:"<<id<<std::endl;
+				// for (int i = 0; i<resultMasks_Num; i++) 
+                //     maskrcnnMask[i*320*240 + y*320 + x] = 0;
+				continue;
+			}
+
+			for (int i = 0; i<resultMasks_Num; i++)
+			{
+				int n = NumMatrix[resultMasks_Num*spNum + id];  // 超像素标号为id的超像素的像素个数
+				int m = NumMatrix[i*spNum + id];  // 第i个mask和第id个超像素的像素重叠数
+				//if(n<filterNumThreshold) continue;
+
+				float test = m*1.0f / n;
+				if (test>0.75)//&&n>filterNumThreshold 超像素内某种标签的mask占了75%以上，就把mask上该超像素内所有像素都置为mask标签值
+				{
+					maskrcnnMask[i*320*240 + y*320 + x] = PixelValueOfEachMask[i];
+				}
+				else
+				{
+					maskrcnnMask[i*320*240 + y*320 + x] = 0;
+				}
+			}
+		}
+	}
+
+
+	free(NumMatrix);
+	free(PixelValueOfEachMask);
 }
